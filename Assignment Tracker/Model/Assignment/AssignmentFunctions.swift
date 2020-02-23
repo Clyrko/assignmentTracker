b5a4e546ce806e4f2c32824a4150ce794bb8d5a5
Jay A <justynadusei@gmail.com>
2020-02-23 16:42:25 +0000

    Updated assignment functions

--- Assignment Tracker/Model/Assignment/AssignmentFunctions.swift
+++ Assignment Tracker/Model/Assignment/AssignmentFunctions.swift
@@ -24,4 +24,33 @@ class AssignmentFunctions {
             CourseData.courseModels[courseIndex].dayModels[dayIndex].assignmentModels.remove(at: index)
         }
     }
+    
+    static func updateAssignment(at courseIndex: Int, oldDayIndex: Int, newDayIndex: Int, using assignmentModel: AssignmentModel) {
+        // Replace with real data store code
+        
+        if oldDayIndex != newDayIndex {
+            // Move assignment to a different Day
+            
+            let lastIndex  = CourseData.courseModels[courseIndex].dayModels[newDayIndex].assignmentModels.count
+            reorderAssignment(at: courseIndex, oldDayIndex: oldDayIndex, newDayIndex: newDayIndex, newAssignmentIndex: lastIndex, assignmentModel: assignmentModel)
+        } else {
+            // Update assignment in the same Day
+            
+            let dayModel = CourseData.courseModels[courseIndex].dayModels[oldDayIndex]
+            let assignmentIndex = (dayModel.assignmentModels.firstIndex(of: assignmentModel))!
+            CourseData.courseModels[courseIndex].dayModels[newDayIndex].assignmentModels[assignmentIndex] = assignmentModel
+        }
+    }
+    
+    static func reorderAssignment(at courseIndex: Int, oldDayIndex: Int, newDayIndex: Int, newAssignmentIndex: Int, assignmentModel: AssignmentModel) {
+        // Replace with real data store code
+        
+        //1. Remove assignment from old location
+        let oldDayModel = CourseData.courseModels[courseIndex].dayModels[oldDayIndex]
+        let oldAssignmentIndex = (oldDayModel.assignmentModels.firstIndex(of: assignmentModel))!
+        CourseData.courseModels[courseIndex].dayModels[oldDayIndex].assignmentModels.remove(at: oldAssignmentIndex)
+        
+        //2. Insert assignment into new location
+        CourseData.courseModels[courseIndex].dayModels[newDayIndex].assignmentModels.insert(assignmentModel, at: newAssignmentIndex)
+    }
 }
